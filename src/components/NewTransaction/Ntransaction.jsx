2021ec389b725915d6ab160060a7655bdc9d9a5a
iamtony <tonnykithui@gmail.com>
2022-04-27 05:51:07 +0300

    Modal close and opening using Framer-motion

--- src/components/NewTransaction/Ntransaction.jsx
+++ src/components/NewTransaction/Ntransaction.jsx
@@ -3,13 +3,30 @@ import TrFrom from './TrFrom'
 import { AiOutlineClose } from "react-icons/ai";
 import { useDispatch } from 'react-redux';
 import { HideModalForm } from '../../redux';
+import { motion } from 'framer-motion';
 
 const Ntransaction = () => {
 
   const dispatch = useDispatch();
 
   return (
-    <div className='Ntransaction-wrap'>
+    <motion.div className='Ntransaction-wrap'
+    animate={{
+      y:0,
+      opacity:1
+    }}
+
+    initial={{
+      y:'100%',
+      opacity:0
+    }}
+
+    transition={{
+      type:'spring',
+      stiffness:"200",
+      duration:2
+    }}
+    >
         <div className="Ntransaction">
             <div className='close'>
                 <div className="close-btn" onClick={() => dispatch(HideModalForm())}>
@@ -19,7 +36,7 @@ const Ntransaction = () => {
             <h2>Add Transaction</h2>
             <TrFrom />
         </div>
-    </div>
+    </motion.div>
   )
 }
 
